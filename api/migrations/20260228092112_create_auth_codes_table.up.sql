0727be4a1c64d4b077a88c525cbf5b3ee24e6339
joegoggin <joegoggin27@gmail.com>
2026-02-28 04:23:25 -0500

    Add `auth_codes` Migrations

---
+++ api/migrations/20260228092112_create_auth_codes_table.up.sql
@@ -0,0 +1,13 @@
+CREATE TYPE code_type AS ENUM ('email_verification', 'password_reset', 'email_change');
+
+CREATE TABLE auth_codes (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    code VARCHAR NOT NULL,
+    code_type code_type NOT NULL,
+    new_email VARCHAR,
+    expires_at TIMESTAMPTZ NOT NULL,
+    used BOOLEAN NOT NULL DEFAULT false,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT now()
+);
+
