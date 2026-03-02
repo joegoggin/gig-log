204fe317b0f4de087a20c9ed01dd113ce5a4323f
joegoggin <joegoggin27@gmail.com>
2026-03-02 02:20:11 -0500

    Fix Typo

--- api/src/core/error.rs
+++ api/src/core/error.rs
@@ -5,27 +5,27 @@ use axum::{
 };
 use gig_log_common::models::error::{ApiError, ValidationError};
 
-pub type ApiResult<T> = Result<T, ApiErrorResposne>;
+pub type ApiResult<T> = Result<T, ApiErrorResponse>;
 
 #[derive(Debug)]
-pub enum ApiErrorResposne {
+pub enum ApiErrorResponse {
     NotFound(String),
     BadRequest(String),
     Validation(Vec<ValidationError>),
     InternalServerError(String),
 }
 
-impl IntoResponse for ApiErrorResposne {
+impl IntoResponse for ApiErrorResponse {
     fn into_response(self) -> Response {
         let (status, message, errors) = match self {
-            ApiErrorResposne::NotFound(msg) => (StatusCode::NOT_FOUND, msg, None),
-            ApiErrorResposne::BadRequest(msg) => (StatusCode::BAD_REQUEST, msg, None),
-            ApiErrorResposne::Validation(errs) => (
+            ApiErrorResponse::NotFound(msg) => (StatusCode::NOT_FOUND, msg, None),
+            ApiErrorResponse::BadRequest(msg) => (StatusCode::BAD_REQUEST, msg, None),
+            ApiErrorResponse::Validation(errs) => (
                 StatusCode::BAD_REQUEST,
                 "Validation failed".to_string(),
                 Some(errs),
             ),
-            ApiErrorResposne::InternalServerError(msg) => {
+            ApiErrorResponse::InternalServerError(msg) => {
                 println!("Error: {:#?}", msg);
 
                 (
@@ -46,18 +46,18 @@ impl IntoResponse for ApiErrorResposne {
     }
 }
 
-impl From<sqlx::Error> for ApiErrorResposne {
+impl From<sqlx::Error> for ApiErrorResponse {
     fn from(err: sqlx::Error) -> Self {
         match err {
             sqlx::Error::RowNotFound => {
-                ApiErrorResposne::NotFound("Resource not found".to_string())
+                ApiErrorResponse::NotFound("Resource not found".to_string())
             }
-            error => ApiErrorResposne::InternalServerError(error.to_string()),
+            error => ApiErrorResponse::InternalServerError(error.to_string()),
         }
     }
 }
 
-impl From<validator::ValidationErrors> for ApiErrorResposne {
+impl From<validator::ValidationErrors> for ApiErrorResponse {
     fn from(errs: validator::ValidationErrors) -> Self {
         let validation_errors = errs
             .field_errors()
@@ -70,6 +70,6 @@ impl From<validator::ValidationErrors> for ApiErrorResposne {
             })
             .collect();
 
-        ApiErrorResposne::Validation(validation_errors)
+        ApiErrorResponse::Validation(validation_errors)
     }
 }
